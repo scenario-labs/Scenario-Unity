6eb48e7ffc5f797be45d89a148caabb42587d94b
Fangh <morgan.klein@6freedom.studio>
2024-02-09 11:08:12 +0100

    feat(tilecreator): tiles are added in the tile palette

--- package/Editor/PromptImages/PromptImagesTileCreator.cs
+++ package/Editor/PromptImages/PromptImagesTileCreator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEditor.Presets;
 using UnityEngine;
@@ -21,6 +22,8 @@ namespace Scenario.Editor
         private PromptImages promptImages;
         private int selectedTextureIndex;
 
+        private bool isProcessing = false;
+
 
         public PromptImagesTileCreator(PromptImages _promptImages, int _selectedTextureIndex)
         {
@@ -34,7 +37,7 @@ namespace Scenario.Editor
 
             if (tilePalette == null)
             {
-                GUILayout.Label("Please set a tile palette to begin.", EditorStyles.wordWrappedLabel);
+                GUILayout.Label("Please set a Tile Palette to begin.", EditorStyles.wordWrappedLabel);
                 return;
             }
 
@@ -49,18 +52,66 @@ namespace Scenario.Editor
                 return;
             }
             GUILayout.Label($"Your Grid layout : {layout}", EditorStyles.wordWrappedLabel);
-            GUILayout.Label($"Generated Tiles will be downloaded in the same folder as your Tile Palette Prefab.", EditorStyles.wordWrappedLabel);
 
-            if(GUILayout.Button(new GUIContent("Download as Tile", "The image will be processed to remove background then downloaded as a sprite in the Scenario Settings save folder. Then a Tile asset will be created (in the same folder as your Tile Palette) out of this Sprite and added to the Tile Palette your referenced.")))
+            if(!isProcessing)
             {
-                promptImages.RemoveBackground(selectedTextureIndex, (imageBytes) =>
+                GUILayout.Label($"Generated Tiles will be downloaded in the same folder as your Tile Palette Prefab.", EditorStyles.wordWrappedLabel);
+
+                if(GUILayout.Button(new GUIContent("Download as Tile", "The image will be processed to remove background then downloaded as a sprite in the Scenario Settings save folder. Then a Tile asset will be created (in the same folder as your Tile Palette) out of this Sprite and added to the Tile Palette your referenced.")))
                 {
-                    CommonUtils.SaveImageDataAsPNG(imageBytes, null, PluginSettings.SpritePreset, (filePath) =>
+                    isProcessing = true;
+                    promptImages.RemoveBackground(selectedTextureIndex, (imageBytes) =>
                     {
-                        Debug.Log(filePath);
+                        CommonUtils.SaveImageDataAsPNG(imageBytes, null, PluginSettings.TilePreset, (spritePath) =>
+                        {
+                            if (PluginSettings.UsePixelsUnitsEqualToImage)
+                            {
+                                CommonUtils.ApplyPixelsPerUnit(spritePath);
+                            }
+
+                            Tile tile = CreateTile(Path.GetDirectoryName(AssetDatabase.GetAssetPath(tilePalette)), (Sprite)AssetDatabase.LoadAssetAtPath(spritePath, typeof(Sprite)));
+                            AddTileToTilePalette(tile);
+                            isProcessing = false;
+                        });
                     });
-                });
+                }
             }
+            else
+            {
+                GUILayout.Label($"Please wait... Image background is being removed. The resulting sprite will be saved in the Scenario Settings save folder. Then a Tile will be created and added in your Tile Palette.", EditorStyles.wordWrappedLabel);
+            }
+
+        }
+
+        private void AddTileToTilePalette(Tile _tile)
+        {
+            Tilemap tilemap = tilePalette.GetComponentInChildren<Tilemap>();
+            Vector3Int emptyTile = Vector3Int.zero;
+            while(tilemap.HasTile(emptyTile))
+            {
+                emptyTile.x += 1;
+                emptyTile.y -= 1;
+            }
+            
+            tilemap.SetTile(emptyTile, _tile);
+        }
+
+        /// <summary>
+        /// Create a tile asset with a specific sprite at a specific path.
+        /// Found here : https://docs.unity3d.com/Manual/Tilemap-ScriptableTiles-Example.html
+        /// </summary>
+        /// <param name="_tilePalettePath"></param>
+        /// <param name="_sprite"></param>
+        private Tile CreateTile(string _tilePalettePath, Sprite _sprite)
+        {
+            Tile newTile = ScriptableObject.CreateInstance<Tile>();
+            newTile.sprite = _sprite;
+            newTile.name = tilePalette.name + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".asset";
+            string filePath = Path.Combine(_tilePalettePath, newTile.name);
+            AssetDatabase.CreateAsset(newTile, filePath);
+            EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(filePath));
+
+            return newTile;
         }
 
     }

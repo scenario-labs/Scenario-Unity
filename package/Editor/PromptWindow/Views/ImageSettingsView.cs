547063453c35b75ad59156ed348d8a5b59fe36a7
mrbusysky <58412572+mrbusysky@users.noreply.github.com>
2025-02-24 04:14:08 -0800

    updated to reflect the values for flux version for settings.

--- package/Editor/PromptWindow/Views/ImageSettingsView.cs
+++ package/Editor/PromptWindow/Views/ImageSettingsView.cs
@@ -33,6 +33,49 @@ namespace Scenario.Editor
                     return;
                 }
 
+                float maxImages = 16f;
+                float maxSteps = 150f;
+                float maxGuidance = 20f;
+
+                if (DataCache.instance.SelectedModelType == "flux.1.1-pro-ultra" || DataCache.instance.SelectedModelType == "flux.1-pro")
+                {
+                    maxImages = 1f;
+                }
+                else if (DataCache.instance.SelectedModelType.StartsWith("flux."))
+                {
+                    maxImages = 8f;
+                }
+
+                if (DataCache.instance.SelectedModelType == "flux.1.1-pro-ultra")
+                {
+                    maxSteps = 0f;
+                }
+                else if (DataCache.instance.SelectedModelType == "flux.1-pro")
+                {
+                    maxSteps = 25f;
+                }
+                else if (DataCache.instance.SelectedModelType.StartsWith("flux."))
+                {
+                    maxSteps = 28f;
+                }
+
+                if (DataCache.instance.SelectedModelType == "flux.1.1-pro-ultra")
+                {
+                    maxGuidance = 0f;
+                }
+                else if (DataCache.instance.SelectedModelType == "flux.1-pro")
+                {
+                    maxGuidance = 3f;
+                }
+                else if (DataCache.instance.SelectedModelType.StartsWith("flux."))
+                {
+                    maxGuidance = 3.5f;
+                }
+
+                imagesliderValue = Mathf.Max(1, Mathf.Min(imagesliderValue, (int)maxImages));
+                samplesliderValue = Mathf.Max(1, Mathf.Min(samplesliderValue, (int)maxSteps));
+                guidancesliderValue = Mathf.Max(0, Mathf.Min(guidancesliderValue, (int)maxGuidance));
+
                 if (!string.IsNullOrEmpty(DataCache.instance.SelectedModelType))
                 {
                     switch (DataCache.instance.SelectedModelType)
@@ -63,79 +106,28 @@ namespace Scenario.Editor
                 if (!DataCache.instance.SelectedModelType.StartsWith("flux."))
                 {
                     EditorGUILayout.BeginHorizontal();
-                    {
-                        GUILayout.Label("Scheduler: ", GUILayout.Width(labelWidth));
-                        promptPusher.schedulerSelected = EditorGUILayout.Popup(promptPusher.schedulerSelected, promptPusher.SchedulerOptions, GUILayout.Width(sliderWidth));
-                    }
-                    EditorGUILayout.EndHorizontal();
-
-                    CustomStyle.Space();
-
-                    EditorGUILayout.BeginHorizontal();
-                    {
-                        GUILayout.Label("Sampling steps: " + samplesliderValue.ToString("00"), GUILayout.ExpandWidth(true), GUILayout.Width(labelWidth));
-                        float maxSteps = 150f;
-                        samplesliderValue = (int)GUILayout.HorizontalSlider(samplesliderValue, 1, maxSteps, GUILayout.Width(sliderWidth));
-                        promptPusher.samplesStep = samplesliderValue;
-                    }
-                    EditorGUILayout.EndHorizontal();
-
-                    EditorGUILayout.BeginHorizontal();
-                    {
-                        GUILayout.Label("Guidance: " + guidancesliderValue.ToString("0.0"), GUILayout.Width(labelWidth));
-                        float maxGuidance = 20f;
-                        guidancesliderValue =
-                            Mathf.Round(GUILayout.HorizontalSlider(guidancesliderValue, 0f, maxGuidance, GUILayout.Width(sliderWidth)) * 10) / 10f;
-                        promptPusher.guidance = guidancesliderValue;
-                    }
+                    GUILayout.Label("Scheduler: ", GUILayout.Width(labelWidth));
+                    promptPusher.schedulerSelected = EditorGUILayout.Popup(promptPusher.schedulerSelected, promptPusher.SchedulerOptions, GUILayout.Width(sliderWidth));
                     EditorGUILayout.EndHorizontal();
                 }
-
 
                 imagesliderIntValue = Mathf.RoundToInt(imagesliderValue);
                 EditorGUILayout.BeginHorizontal();
-                {
-                    GUILayout.Label("Images: " + imagesliderIntValue, GUILayout.Width(labelWidth));
-                    float maxImages = 16f;
-                    if (DataCache.instance.SelectedModelType == "flux.1.1-pro-ultra" || DataCache.instance.SelectedModelType == "flux.1-pro")
-                    {
-                        maxImages = 1f;
-                    }
-                    else if (DataCache.instance.SelectedModelType.StartsWith("flux."))
-                    {
-                        maxImages = 8f;
-                    }
-                    else
-                    {
-                        maxImages = 16f;
-                    }
-                    imagesliderValue = GUILayout.HorizontalSlider(imagesliderValue, 1, maxImages, GUILayout.Width(sliderWidth));
-                    promptPusher.numberOfImages = Mathf.RoundToInt(imagesliderValue);
-                }
-                EditorGUILayout.EndHorizontal();
-
-
-                EditorGUILayout.BeginHorizontal();
-                {
-                    GUILayout.Label("Sampling steps: " + samplesliderValue.ToString("00"), GUILayout.ExpandWidth(true), GUILayout.Width(labelWidth));
-                    float maxSteps = 150f;
-                    if (DataCache.instance.SelectedModelType.StartsWith("flux."))
-                        maxSteps = 28f;
-                    samplesliderValue = (int)GUILayout.HorizontalSlider(samplesliderValue, 1, maxSteps, GUILayout.Width(sliderWidth));
-                    promptPusher.samplesStep = samplesliderValue;
-                }
+                GUILayout.Label("Images: " + imagesliderIntValue, GUILayout.Width(labelWidth));
+                imagesliderValue = GUILayout.HorizontalSlider(imagesliderValue, 1, maxImages, GUILayout.Width(sliderWidth));
+                promptPusher.numberOfImages = Mathf.RoundToInt(imagesliderValue);
                 EditorGUILayout.EndHorizontal();
 
                 EditorGUILayout.BeginHorizontal();
-                {
-                    GUILayout.Label("Guidance: " + guidancesliderValue.ToString("0.0"), GUILayout.Width(labelWidth));
-                    float maxGuidance = 20f;
-                    if (DataCache.instance.SelectedModelType.StartsWith("flux."))
-                        maxGuidance = 3.5f;
-                    guidancesliderValue =
-                        Mathf.Round(GUILayout.HorizontalSlider(guidancesliderValue, 0f, maxGuidance, GUILayout.Width(sliderWidth)) * 10) / 10f;
-                    promptPusher.guidance = guidancesliderValue;
-                }
+                GUILayout.Label("Sampling steps: " + samplesliderValue.ToString("00"), GUILayout.Width(labelWidth));
+                samplesliderValue = (int)GUILayout.HorizontalSlider(samplesliderValue, 1, maxSteps, GUILayout.Width(sliderWidth));
+                promptPusher.samplesStep = samplesliderValue;
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Label("Guidance: " + guidancesliderValue.ToString("0.0"), GUILayout.Width(labelWidth));
+                guidancesliderValue = Mathf.Round(GUILayout.HorizontalSlider(guidancesliderValue, 0f, maxGuidance, GUILayout.Width(sliderWidth)) * 10) / 10f;
+                promptPusher.guidance = guidancesliderValue;
                 EditorGUILayout.EndHorizontal();
 
                 if (promptWindow.ActiveMode != null)
@@ -143,30 +135,26 @@ namespace Scenario.Editor
                     if (promptWindow.ActiveMode.EMode == ECreationMode.Image_To_Image || promptWindow.ActiveMode.IsControlNet)
                     {
                         EditorGUILayout.BeginHorizontal();
-                        {
-                            GUILayout.Label(new GUIContent("Influence: " + influenceSliderValue.ToString("F0"),"Higher values amplify the weight of the reference image, affecting the final output."), GUILayout.Width(labelWidth));
-                            influenceSliderValue = (int)GUILayout.HorizontalSlider(influenceSliderValue, 0, 99, GUILayout.Width(sliderWidth));
-                            promptPusher.influenceOption = influenceSliderValue;
-                        }
+                        GUILayout.Label(new GUIContent("Influence: " + influenceSliderValue.ToString("F0"), "Higher values amplify the weight of the reference image, affecting the final output."), GUILayout.Width(labelWidth));
+                        influenceSliderValue = (int)GUILayout.HorizontalSlider(influenceSliderValue, 0, 99, GUILayout.Width(sliderWidth));
+                        promptPusher.influenceOption = influenceSliderValue;
                         EditorGUILayout.EndHorizontal();
                     }
                 }
 
                 EditorGUILayout.BeginHorizontal();
+                GUILayout.Label("Seed", GUILayout.Width(labelWidth));
+                if (shouldAutoGenerateSeed)
                 {
-                    GUILayout.Label("Seed", GUILayout.Width(labelWidth));
-                    if (shouldAutoGenerateSeed)
-                    {
-                        GUI.enabled = false;
-                        seedinputText = GUILayout.TextField(seedinputText, GUILayout.Height(20), GUILayout.Width(sliderWidth));
-                        promptWindow.SetSeed(/*seedinputText == "-1" ? null : */seedinputText);
-                    }
-                    else
-                    {
-                        GUI.enabled = true;
-                        seedinputText = GUILayout.TextField(seedinputText, GUILayout.Height(20), GUILayout.Width(sliderWidth));
-                        promptWindow.SetSeed(/*seedinputText == "-1" ? null : */seedinputText);
-                    }
+                    GUI.enabled = false;
+                    seedinputText = GUILayout.TextField(seedinputText, GUILayout.Height(20), GUILayout.Width(sliderWidth));
+                    promptWindow.SetSeed(seedinputText);
+                }
+                else
+                {
+                    GUI.enabled = true;
+                    seedinputText = GUILayout.TextField(seedinputText, GUILayout.Height(20), GUILayout.Width(sliderWidth));
+                    promptWindow.SetSeed(seedinputText);
                 }
                 EditorGUILayout.EndHorizontal();
             }
@@ -181,11 +169,11 @@ namespace Scenario.Editor
         private void DrawSliderSizeValue(int[] _allowedValues)
         {
             CustomStyle.Space(10);
-
+            
             EditorGUILayout.BeginHorizontal();
             {
                 GUILayout.Label("Size: " + promptPusher.width + " x " + promptPusher.height, GUILayout.Width(labelWidth));
-                sizeSliderValue = GUILayout.HorizontalSlider(sizeSliderValue, 1, (_allowedValues.Length *2)-1, GUILayout.Width(sliderWidth)) ;
+                sizeSliderValue = GUILayout.HorizontalSlider(sizeSliderValue, 1, (_allowedValues.Length * 2) - 1, GUILayout.Width(sliderWidth));
                 sizeSliderValue = Mathf.Round(sizeSliderValue);
                 int correspondingValue = 0;
 

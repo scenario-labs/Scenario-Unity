a3a3ef9ad888eeab3eecf46b64a3828c05b473b4
mrbusysky <58412572+mrbusysky@users.noreply.github.com>
2025-02-26 00:29:06 -0800

    moved this out of the flux.

--- package/Editor/PromptWindow/PromptWindowUI.cs
+++ package/Editor/PromptWindow/PromptWindowUI.cs
@@ -277,22 +277,8 @@ namespace Scenario.Editor
                     if (!string.IsNullOrEmpty(DataCache.instance.SelectedModelType) &&
                         DataCache.instance.SelectedModelType.StartsWith("flux.", StringComparison.OrdinalIgnoreCase))
                     {
-                        if (DataCache.instance.SelectedModelId == "flux.1-schnell") 
-                        {
-                            if (eMode == ECreationMode.Text_To_Image || 
-                                eMode == ECreationMode.Image_To_Image ||
-                                eMode == ECreationMode.ControlNet ||
-                                eMode == ECreationMode.IP_Adapter ||
-                                eMode == ECreationMode.Image_To_Image__ControlNet ||
-                                eMode == ECreationMode.Image_To_Image__IP_Adapter ||
-                                eMode == ECreationMode.ControlNet__IP_Adapter)
-                            {
-                                availableModes.Add(eMode);
-                                string eName = eMode.ToString("G").Replace("__", " + ").Replace("_", " ");
-                                tabLabels.Add(eName);
-                            }
-                        }
-                        else if (DataCache.instance.SelectedModelType.Equals("flux1.1-pro", StringComparison.OrdinalIgnoreCase) ||
+                        
+                        if (DataCache.instance.SelectedModelType.Equals("flux1.1-pro", StringComparison.OrdinalIgnoreCase) ||
                                 DataCache.instance.SelectedModelType.Equals("flux.1-pro", StringComparison.OrdinalIgnoreCase))
                         {
                             if (eMode == ECreationMode.Text_To_Image)
@@ -314,6 +300,21 @@ namespace Scenario.Editor
                             continue; 
                         }
                         else
+                        {
+                            availableModes.Add(eMode);
+                            string eName = eMode.ToString("G").Replace("__", " + ").Replace("_", " ");
+                            tabLabels.Add(eName);
+                        }
+                    }
+                    else if (DataCache.instance.SelectedModelId == "flux.1-schnell") 
+                    {
+                        if (eMode == ECreationMode.Text_To_Image || 
+                            eMode == ECreationMode.Image_To_Image ||
+                            eMode == ECreationMode.ControlNet ||
+                            eMode == ECreationMode.IP_Adapter ||
+                            eMode == ECreationMode.Image_To_Image__ControlNet ||
+                            eMode == ECreationMode.Image_To_Image__IP_Adapter ||
+                            eMode == ECreationMode.ControlNet__IP_Adapter)
                         {
                             availableModes.Add(eMode);
                             string eName = eMode.ToString("G").Replace("__", " + ").Replace("_", " ");
